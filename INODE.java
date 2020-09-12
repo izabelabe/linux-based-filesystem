0c1d8efd30871b01f64849c9794ead8d1221fe45
izabelabe <58079659+izabelabe@users.noreply.github.com>
2020-09-13 00:03:49 +0200

    Add files via upload

---
+++ INODE.java
@@ -0,0 +1,13 @@
+package projekt_SO;
+
+public class INODE {
+int size;
+int pointer;
+long i_ctime;
+long i_mtime;
+int block_index1;
+int block_index2;
+int index_block; 
+	
+
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package projekt_SO;
+import java.io.IOException;
+
+public class Main {
+
+	
+		public static void main(String[] args) throws IOException, FileException {
+			Shell object = new Shell();
+			
+			object.boot();
+		}
+	
+
+}
